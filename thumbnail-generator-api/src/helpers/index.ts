1d73395ccf9666b82aaab9d1199f0c0f9b3c63b6
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2023-03-21 18:52:13 -0300

    untrack thumbnails

--- thumbnail-generator-api/src/helpers/index.ts
+++ thumbnail-generator-api/src/helpers/index.ts
@@ -1,7 +1,12 @@
 import sharp from "sharp"
+import fs from "fs";
 
 export const resizeImage = async (width: number, height: number, path: string, name: string, outputName?: string) => {
   const newPath = `${process.cwd()}\\thumbnails\\${outputName || name}.png`;
+
+  if (!fs.existsSync(`${process.cwd()}\\thumbnails`)) {
+    fs.mkdirSync(`${process.cwd()}\\thumbnails`);
+  }
   const thumbnail = await sharp(path)
   .resize(width, height, {
     fit: 'cover',
